2e3878a60dab7d7914ddc21ac22eea2989fb393c
Juks0 <129502580+Juks0@users.noreply.github.com>
2025-04-06 22:19:07 +0200

    test 6

--- swiper.js
+++ swiper.js
@@ -1,40 +1,45 @@
 let buttons = document.querySelector('#swiper-button-prev')
 
 let swiper = new Swiper(".mySwiper", {
-    cssMode: true,
+    // cssMode: true,
     loop: true,
-    spaceBetween: 10,
+    allowTouchMove: true,
     overflow: 'hidden',
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
     autoplay: {
         delay: 4000,
         disableOnInteraction: false,
     },
-    mousewheel: true,
-    keyboard: true,
+    // mousewheel: true,
+    // keyboard: true,
     breakpoints: {
         0: {
             slidesPerView:1,
-            spaceBetween: 20,
+            spaceBetween: 0,
         },
         500: {
             slidesPerView:2,
-            spaceBetween: 20,
+            spaceBetween: 5,
+        },
+        768:{
+            slidesPerView:2,
+            spaceBetween: 5,
+            // allowTouchMove: false,
         },
         780: {
             slidesPerView: 3,
-            spaceBetween: 20,
+            spaceBetween: 8,
         },
         1450: {
             slidesPerView: 4,
-            spaceBetween: 40,
+            spaceBetween: 10,
         },
     }
 })
